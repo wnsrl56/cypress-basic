5877dc24645401f3c07cafd88d9a3f1ec8c936b7
Jun <wnsrl56@gmail.com>
2021-04-09 15:38:48 +0900

    feat: event 로직 추가

--- src/js/modules/Counter.js
+++ src/js/modules/Counter.js
@@ -5,14 +5,38 @@ export default function Counter({ $app }) {
           <h1>ui counter</h1>
           <div class="counter">
             <a href="#" class="minus-button"><span>-</span></a>
-            <input name="count" type="text" class="count-display" value="10">
+            <input readonly name="count" type="text" class="count-display" value="10">
             <a href="#" class="plus-button"><span>+</span></a>
           </div>
         </div>`;
   };
 
+  const appendEvents = ({ increaseValue, decreaseValue }) => {
+    const plusEl = document.querySelector('.plus-button');
+    const minusEl = document.querySelector('.minus-button');
+
+    plusEl.addEventListener('click', increaseValue);
+    minusEl.addEventListener('click', decreaseValue);
+  };
+
+  const increaseValue = () => {
+    const inputEl = document.querySelector('.count-display');
+    const value = Number(inputEl.value);
+    
+    if(value >= 12) return;
+    inputEl.value = value + 1;
+  }
+
+  const decreaseValue = () => {
+    const inputEl = document.querySelector('.count-display');
+    const value = Number(inputEl.value);
+
+    if(value <= 8) return;
+    inputEl.value = value - 1;
+  }
   const init = () => {
     render();
+    appendEvents({ increaseValue, decreaseValue });
   };
 
   init();
